404a19f9e20840b0591945c2b7408f54bb74c7b4
tkouts <tkouts@innoscript.org>
2009-04-16 16:05:35 +0300

    minor optimizations

--- modpython/porcupine.py
+++ modpython/porcupine.py
@@ -1,38 +1,35 @@
 "Porcupine MOD_PYTHON connector"
-
-import socket, sys, ConfigParser, os
+import socket
+import sys
+import ConfigParser
+import os
 import re
+import itertools
 from errno import EISCONN, EADDRINUSE
 from threading import RLock
 from cPickle import dumps, loads
 from mod_python import apache
 
 BUFSIZ = 8*1024
-HTMLCodes = [
-    ['&', '&amp;'],
-    ['<', '&lt;'],
-    ['>', '&gt;'],
-    ['"', '&quot;'],
-]
+HTMLCodes = (('&', '&amp;'),
+             ('<', '&lt;'),
+             ('>', '&gt;'),
+             ('"', '&quot;'))
 IP_ADDR = socket.gethostbyname(socket.gethostname())
 MOBILE_BROWSER_SIGNATURE = re.compile('MIDP|UNTRUSTED')
-PORT_RANGE = range(65535, 40958, -1)
-NEXT_HOST_LOCK = RLock()
 #WEB_LOG = open('C:/www/web.log', 'w+')
 
 class Host(object):
+    port = itertools.cycle(range(65535, 40958, -1))
+    
     def __init__(self, address):
         self.address = address
         self.connections = 0
         self.tot = 0
-        self.port = self.getPort()
-
-    def getPort(self):
-        while True:
-            for port in PORT_RANGE:
-                yield(port)
 
 class Site(object):
+    _lock = RLock()
+    
     def __init__(self):
         self.isPopulated = False
 
@@ -56,22 +53,24 @@ class Site(object):
         return tuple(address)
         
     def getNextHost(self):
-        # round robin
-        NEXT_HOST_LOCK.acquire()
-        next = self.__rrcounter = (self.__rrcounter + 1) % len(self.__hosts)
-        NEXT_HOST_LOCK.release()
-        return self.__hosts[next:] + self.__hosts[0:next]
-        # least connections
-#        self.__hosts.sort(self.sort)
-#        return(self.__hosts)
-#        NEXT_HOST_LOCK.acquire()
-#        conns = [host.connections for host in self.__hosts]
-#        NEXT_HOST_LOCK.release()
-#        pairs = zip(conns, self.__hosts)
-#        pairs.sort()
-#        hosts = [x[1] for x in pairs]
-#        return(hosts)
-            
+        if len(self.__hosts) == 1:
+            return self.__hosts
+        else:
+            # round robin
+            self._lock.acquire()
+            next = self.__rrcounter = (self.__rrcounter + 1) % len(self.__hosts)
+            self._lock.release()
+            return self.__hosts[next:] + self.__hosts[0:next]
+        ## least connections
+        ##self.__hosts.sort(self.sort)
+        ##return(self.__hosts)
+        ##NEXT_HOST_LOCK.acquire()
+        ##conns = [host.connections for host in self.__hosts]
+        ##NEXT_HOST_LOCK.release()
+        ##pairs = zip(conns, self.__hosts)
+        ##pairs.sort()
+        ##hosts = [x[1] for x in pairs]
+        ##return(hosts)
 
 SITE=Site()
 inifile = os.path.dirname(__file__) + '/server.ini'
@@ -81,66 +80,56 @@ def handler(req):
     req.add_common_vars()
     # construct the environment dictionary
     environ = {}
-    for sVar in req.subprocess_env.keys():
-        environ[sVar] = req.subprocess_env[sVar]
-
-    return handleRequest(req, environ)            
-
+    environ.update(req.subprocess_env)
+    return handleRequest(req, environ)
 
 def handleRequest(req, environ):
     try:
-        # get input
-        requestBody = req.read()
         dict = {
             'if': 'MOD_PYTHON',
             'env': environ,
-            'inp': requestBody
+            'inp': req.read()
         }
         data = dumps(dict, 2)
-
-        while True:
-            hosts = SITE.getNextHost()
-
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            try:
-                for host in hosts:
-                    err = s.connect_ex(host.address)
-                    while not err in (0, EISCONN):
-                        if err == EADDRINUSE:  # address already in use
-                            # the ephemeral port range is exhausted
-                            s.close()
-                            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                            s.bind((IP_ADDR, host.port.next()))
-                        else:
-                            # the host refuses conncetion
-                            break
-                        err = s.connect_ex(host.address)
+        hosts = SITE.getNextHost()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            response = []
+            for host in hosts:
+                err = s.connect_ex(host.address)
+                while not err in (0, EISCONN):
+                    if err == EADDRINUSE:  # address already in use
+                        # the ephemeral port range is exhausted
+                        s.close()
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        s.bind((IP_ADDR, host.port.next()))
                     else:
-                        # we got a connection
-                        host.connections += 1
-                        host.tot += 1
+                        # the host refuses conncetion
                         break
+                    err = s.connect_ex(host.address)
+                else:
+                    # we got a connection
+                    host.connections += 1
+                    host.tot += 1
+                    break
 
-                # Send our request to Porcupine Server
-                s.send(data)
-                s.shutdown(1)
+            # Send our request to Porcupine Server
+            s.send(data)
+            s.shutdown(1)
 
-                # Get the response object from Porcupine Server
-                response = []
-                while True:
-                    rdata = s.recv(BUFSIZ)
-                    if not rdata:
-                        response = ''.join(response)
-                        break
-                    response.append(rdata)
-                break
-            finally:
-                s.close()
-                host.connections -= 1
-                #WEB_LOG.write('closing connection for host %s. Total conns:%d\n' %(host.address, host.tot))
-                #WEB_LOG.flush()
+            # Get the response object from Porcupine Server
+            while True:
+                rdata = s.recv(BUFSIZ)
+                if not rdata:
+                    break
+                response.append(rdata)
+        finally:
+            s.close()
+            host.connections -= 1
+            #WEB_LOG.write('closing connection for host %s. Total conns:%d\n' %(host.address, host.tot))
+            #WEB_LOG.flush()
 
+        response = ''.join(response)
         ret_code, body, headers, cookies = loads(response)
         req.status = ret_code
         
@@ -156,7 +145,7 @@ def handleRequest(req, environ):
             req.write(body)
         else:
             # it is a redirect
-            if MOBILE_BROWSER_SIGNATURE.search( environ['HTTP_USER_AGENT'] ):
+            if MOBILE_BROWSER_SIGNATURE.search(environ['HTTP_USER_AGENT']):
                 # it is a mobile device
                 # handle redirect internally
                 sLocation = headers['Location']
@@ -169,7 +158,7 @@ def handleRequest(req, environ):
                 if len(lstPath)==2:
                     environ['QUERY_STRING'] = lstPath[1]
                 return handleRequest(req, environ) 
-            else:                
+            else:
                 req.headers_out['Location'] = headers['Location']
                 req.send_http_header()
         
@@ -187,18 +176,18 @@ def handleRequest(req, environ):
         output = ''.join(output)
         output = HTMLEncode(output)
         req.content_type = 'text/html'
-        req.write('''
-<html><body>
-<H3>Porcupine Server</H3>
-<p>
-<pre>
-ERROR
+        req.write('''<html>
+    <body>
+        <H3>Porcupine Server</H3>
+        <p>
+            <pre>
+                ERROR
 
-%s
-</pre>
-</p>
-</body></html>
-''' % output)
+                %s
+            </pre>
+        </p>
+    </body>
+</html>''' % output)
         return apache.OK
 
 def HTMLEncode(s, codes=HTMLCodes):
